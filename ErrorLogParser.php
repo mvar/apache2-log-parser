8f3838c51ebd8281f9f6ff6b8e9a0dc9975abd29
Mantas Varatiejus <var.mantas@gmail.com>
2013-12-29 23:43:54 +0200

    License information

--- ErrorLogParser.php
+++ ErrorLogParser.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * (c) Mantas Varatiejus <var.mantas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace MVar\Apache2LogParser;
 
+/**
+ * Apache 2.2 and older error log parser
+ *
+ * @package MVar\Apache2LogParser
+ */
 class ErrorLogParser implements ParserInterface
 {
     /**
--- Tests/ErrorLogParserTest.php
+++ Tests/ErrorLogParserTest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * (c) Mantas Varatiejus <var.mantas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace MVar\Apache2LogParser\Tests;
 
