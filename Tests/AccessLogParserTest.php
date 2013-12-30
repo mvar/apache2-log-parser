2c173c623e0fbb0583624d6f9b1b68b03c86b12f
Mantas Varatiejus <var.mantas@gmail.com>
2013-12-30 22:35:30 +0200

    More combined log tests

--- Tests/AccessLogParserTest.php
+++ Tests/AccessLogParserTest.php
@@ -70,6 +70,57 @@ class AccessLogParserTest extends \PHPUnit_Framework_TestCase
                     'user_agent' => 'Mozilla/4.08 [en] (Win98; I ;Nav)',
                 )
             ),
+            array(
+                AccessLogParser::FORMAT_COMBINED,
+                '127.0.0.1 - - [28/Dec/2013:19:03:49 +0200] "GET /test-page/ HTTP/1.1" 200 8359 "-" "Symfony2 BrowserKit"',
+                array(
+                    'client_ip' => '127.0.0.1',
+                    'identity' => '-',
+                    'user_id' => '-',
+                    'time' => '2013-12-28T19:03:49+0200',
+                    'request_method' => 'GET',
+                    'request_file' => '/test-page/',
+                    'request_protocol' => 'HTTP/1.1',
+                    'response_code' => '200',
+                    'response_body_size' => '8359',
+                    'referer' => '-',
+                    'user_agent' => 'Symfony2 BrowserKit',
+                )
+            ),
+            array(
+                AccessLogParser::FORMAT_COMBINED,
+                '66.249.78.230 - - [29/Dec/2013:16:07:58 +0200] "GET /robots.txt HTTP/1.1" 200 408 "-" "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"',
+                array(
+                    'client_ip' => '66.249.78.230',
+                    'identity' => '-',
+                    'user_id' => '-',
+                    'time' => '2013-12-29T16:07:58+0200',
+                    'request_method' => 'GET',
+                    'request_file' => '/robots.txt',
+                    'request_protocol' => 'HTTP/1.1',
+                    'response_code' => '200',
+                    'response_body_size' => '408',
+                    'referer' => '-',
+                    'user_agent' => 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+                )
+            ),
+            array(
+                AccessLogParser::FORMAT_COMBINED,
+                '71.82.1.1 - - [29/Dec/2013:17:37:40 +0200] "GET / HTTP/1.1" 200 2577 "http://example.com/test/" "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"',
+                array(
+                    'client_ip' => '71.82.1.1',
+                    'identity' => '-',
+                    'user_id' => '-',
+                    'time' => '2013-12-29T17:37:40+0200',
+                    'request_method' => 'GET',
+                    'request_file' => '/',
+                    'request_protocol' => 'HTTP/1.1',
+                    'response_code' => '200',
+                    'response_body_size' => '2577',
+                    'referer' => 'http://example.com/test/',
+                    'user_agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+                )
+            ),
         );
     }
 }
